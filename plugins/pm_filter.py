62e0d3f6d777e189abbfa758f73e075441860f13
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2022-04-27 07:48:21 +0530

    Bold the movie not found in database

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbyAdminBot⚡')
+            k = await query.message.edit('<b>⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbyAdminBot⚡</b>')
             await asyncio.sleep(10)
             await k.delete()
 
