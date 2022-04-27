5574881b1409361b05af3c5562c5c07ef374a1c6
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2022-04-27 07:11:06 +0530

    Not available to admin bot
    Change movie not available in database base to contact admin bot

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase')
+            k = await query.message.edit('⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbyAdminBot⚡')
             await asyncio.sleep(10)
             await k.delete()
 
