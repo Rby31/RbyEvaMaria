ed98a95adf1b789891c1e5262d73a4f464b04add
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2022-04-30 16:48:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,7 +120,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("For Latest Movies ... 😜/n/n    ✪༺ ──•◈•─ ─•◈•──༻✪/n/n    ♻️ Channel Link  : @Playit_Linkz⚡", show_alert=True)
+        return await query.answer("For Latest Movies ... 😜                                       ♻️ Channel : ⚡ @Playit_Linkz ⚡", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
