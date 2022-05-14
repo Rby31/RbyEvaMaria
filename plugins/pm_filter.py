c780fbe9451ce076b9c5b1378e86eb8b2c421352
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2022-05-14 11:38:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('<b>⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbyAdminBot⚡</b>')
+            k = await query.message.edit('<b>⚠️Sorry !!! This Movie Is Not Available !\n\n Please Msg To ...\n\n ✌️Admin  ➠  @RbY_Rj ⚡</b>')
             await asyncio.sleep(10)
             await k.delete()
 
